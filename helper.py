a80c37129e8ee4b569e9ae7bd46094e92df95de0
Francis Donahue <fmarcelodonahue@gmail.com>
2021-09-27 11:31:33 -0500

    Change to if-else in selecting manufacturer class

--- helper.py
+++ helper.py
@@ -1,10 +1,6 @@
 from manufacturers.ASUS.asus_helpers import GetASUSDrivers
 
 def manufacturer_class(board_maker, model, os_info):
-    board_manufacturers = {
-        'ASUSTeK COMPUTER INC.': GetASUSDrivers(model, os_info)
-    }
-    return_data = board_manufacturers.get(board_maker)
-    if return_data:
-        return return_data
+    if board_maker == 'ASUSTeK COMPUTER INC.':
+        return GetASUSDrivers(model, os_info)
     raise NotImplementedError(f'Tracking drivers of {board_maker} manufacturer has not yet implemented.')

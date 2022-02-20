cd262d1e0dcde0f54337df304f146cd7ff7a77de
Francis Donahue <fmarcelodonahue@gmail.com>
2022-02-19 19:32:07 -0600

    Remove files

--- server/manufacturers/ASUS/TUF.py
+++
@@ -1,8 +0,0 @@
-from ASUS import ASUS
-
-class TUF(ASUS):
-    def __init__(self, model, os_info):
-        super().__init__(model, os_info)
-        model_list = model.split()
-        self.product_series = "-".join(model_list[:2])
-        self.url = f'/Motherboards-Components/Motherboards/{self.product_series}/{"-".join(model_list)}/HelpDesk_Download/'

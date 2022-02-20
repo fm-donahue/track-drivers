a30c429dba7dd6e435e95f59f9642c6a993ffe02
Francis Donahue <fmarcelodonahue@gmail.com>
2022-02-19 19:01:42 -0600

    Move to server folder

--- server/helper.py
+++ server/helper.py
@@ -1,6 +1,6 @@
 from manufacturers.ASUS.asus_helpers import GetASUSDrivers
 
-def manufacturer_class(board_maker, model, os_info):
+def manufacturer_class(board_maker, model, os_info, session):
     if board_maker == 'ASUSTeK COMPUTER INC.':
-        return GetASUSDrivers(model, os_info)
+        return GetASUSDrivers(model, os_info, session)
     raise NotImplementedError(f'Tracking drivers of {board_maker} manufacturer has not yet implemented.')
